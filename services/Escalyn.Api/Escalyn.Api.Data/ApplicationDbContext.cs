5253b4d8f241a6ae0f91ddce9186d121022147ca
Mert_1026 <mertelsenev10@gmail.com>
2026-02-09 14:52:47 +0200

    Initial

---
+++ services/Escalyn.Api/Escalyn.Api.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escalyn.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // Define your DbSets here
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
