244c73be6b4759bb8c86565a033de2ee0ef75333
Mert_1026 <mertelsenev10@gmail.com>
2026-02-13 17:53:32 +0200

    Added some dto models associated with the cases

---
+++ services/api/Escalyn.Api.Data.Models/DTOs/CaseStatusUpdateDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escalyn.Api.Data.Models.DTOs
+{
+    public class CaseStatusUpdateDTO
+    {
+        public Guid CaseId { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public string CompanyEmail { get; set; }
+        public List<QuestionDTO> Questions { get; set; } = new List<QuestionDTO>();
+
+    }
+}
